2b2ffa6c1774052112d595407b43de7163e62f86
Jack Holdsworth <jackch90@gmail.com>
2020-05-11 11:32:31 +0100

    Mongoose connection and model

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const dotenv = require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-const mongoose = require('mongoose');
 const sensorLib = require('node-dht-sensor');
 
-
+app.use(express.static('public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlEncoded({extended: false}));
 
 // Sensor reading every two seconds
 function querySensor() {
@@ -18,9 +19,26 @@ function querySensor() {
         }
     })
 }
-querySensor();
 
+// Mongoose Database connection
+const mongoose = require('mongoose');
+const url = process.env.MONGO_DB
+mongoose
+    .connect(url, {useNewUrlParser: true})
+    .then('open', () => {
+        console.log('MongoDB connected on: ' + url);
+    })
+    .catch('error', (err) => {
+        console.log('Error: ' + err);
+    });
+// Mongoose model creation
 
+const reading = new mongoose.model('Reading', {
+    temperature: String,
+    humidity: String
+})
+
+// Server connection and routing
 app.listen(port, () => {
     console.log('Connected on port: ' + port);
 })
