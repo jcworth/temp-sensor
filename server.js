325078df253ef6ada9beae5d4198411ad82a2b62
Jack Holdsworth <jackch90@gmail.com>
2020-05-11 11:19:32 +0100

    Sensor reading on server

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose');
+const sensorLib = require('node-dht-sensor');
+
+
+
+// Sensor reading every two seconds
+function querySensor() {
+    sensorLib.read(22, 4, (err, temperature, humidity) => {
+        if (err) {
+            console.warn(err);
+        } else {
+            console.log(Date() + `\nTemperature: ${temperature}°C, Humidity: ${humidity}%`);
+            setTimeout(querySensor, 2000);
+        }
+    })
+}
+querySensor();
+
 
 app.listen(port, () => {
     console.log('Connected on port: ' + port);
